1b6d79578ca9370fb34396c3451b370fe18af0c9
Italo <italocmelo_@hotmail.com>
2020-03-31 12:40:24 -0300

    melhoria codigo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var webservice = require('./config/api.js');
 var app = require("./config/server.js");
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-var casos = 4371, casosHoje, mortes = 141, mortesHoje, curados, doentes, criticos;
+var casos = 4685, casosHoje, mortes = 168, mortesHoje, curados, doentes, criticos;
 
 app.listen(server_port, server_host, function () {
     console.log("Aplicação online.");
@@ -28,19 +28,18 @@ function getCorona() {
                         } else {
                             tweet = novasmortes + " novas mortes pelo coronavírus no Brasil :(\nTotal de mortes:" + json.deaths + "\nNovos casos: " + novoscasos + "\nTotal de casos: " + json.cases;
                         }
-                        mortes = json.deaths;
-                        casos = json.cases;
+
                     } else if (mortes < json.deaths) {
                         var novasmortes = (json.deaths - mortes)
                         tweet = novasmortes + " novas mortes pelo coronavírus no Brasil :(\nTotal de mortes : " + json.deaths + "\nTotal de casos: " + json.cases;
-                        mortes = json.deaths;
                     } else {
                         var novoscasos = (json.cases - casos)
                         tweet = (novoscasos + " novos casos de coronavírus no Brasil :(" + "\nTotal de casos : " + json.cases)
-                        casos = json.cases
                     }
                     console.log(tweet);
                     cliente.tweetar(tweet);
+                    mortes = json.deaths;
+                    casos = json.cases;
                 }
             })
             .catch(error => {
