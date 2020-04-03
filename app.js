2e5520c594aabe4e4de35eac2a73f2b4bd41e621
Italo <italord.melo@gmail.com>
2020-04-03 11:33:04 -0300

    data.js gerando automaticamente

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const fs = require('fs');
 
 app.listen(server_port, server_host, function () {
     console.log("Aplicação online.");
+
+    if (fs.existsSync('data.json')) {
+    } else {
+        var zerado = "{\"casos\":0,\"mortes\":0}"
+        fs.writeFileSync('data.json', zerado);
+    }
 });
 
 function getCorona() {
@@ -26,17 +32,17 @@ function getCorona() {
                         var novasmortes = (json.deaths - dados.mortes)
                         var novoscasos = (json.cases - dados.casos)
                         if (novasmortes == 1) {
-                            tweet = novasmortes + " nova morte pelo coronavírus no Brasil :(\nTotal de mortes: " + json.deaths + " 💀\nNovos casos: " + novoscasos + "\nTotal de casos: " + json.cases +" 😷";
+                            tweet = novasmortes + " nova morte pelo coronavírus no Brasil :(\nTotal de mortes: " + json.deaths + " 💀\nNovos casos: " + novoscasos + "\nTotal de casos: " + json.cases + " 😷";
                         } else {
-                            tweet = novasmortes + " novas mortes pelo coronavírus no Brasil :(\nTotal de mortes:" + json.deaths + " 💀\nNovos casos: " + novoscasos + "\nTotal de casos: " + json.cases +" 😷";
+                            tweet = novasmortes + " novas mortes pelo coronavírus no Brasil :(\nTotal de mortes:" + json.deaths + " 💀\nNovos casos: " + novoscasos + "\nTotal de casos: " + json.cases + " 😷";
                         }
 
                     } else if (dados.mortes < json.deaths) {
                         var novasmortes = (json.deaths - dados.mortes)
-                        tweet = novasmortes + " novas mortes pelo coronavírus no Brasil :(\nTotal de mortes : " + json.deaths + " 💀\nTotal de casos: " + json.cases +" 😷";
+                        tweet = novasmortes + " novas mortes pelo coronavírus no Brasil :(\nTotal de mortes : " + json.deaths + " 💀\nTotal de casos: " + json.cases + " 😷";
                     } else {
                         var novoscasos = (json.cases - dados.casos)
-                        tweet = (novoscasos + " novos casos de coronavírus no Brasil :(" + "\nTotal de casos : " + json.cases + " 😷 \nTotal de mortes : " + json.deaths+ " 💀")
+                        tweet = (novoscasos + " novos casos de coronavírus no Brasil :(" + "\nTotal de casos : " + json.cases + " 😷 \nTotal de mortes : " + json.deaths + " 💀")
                     }
                     console.log(tweet);
                     cliente.tweetar(tweet);
